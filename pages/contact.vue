20ef0bb7d6a66787ef50e0d29a3685ee730cbc88
Treana Slocum <treanaslocum.com>
2022-06-24 13:43:51 -0700

    Add setmore integration

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,18 +7,25 @@
       <p class="mb-2 uppercase font-bold text-sm sm:text-2xl">Drop us a line</p>
       <h2 class="font-medium text-2xl sm:text-5xl text-center">Let’s start a project together.</h2>
       <p class="mt-12 mb-32 w-2/3 sm:w-1/2 text-dark-gray  text-lg sm:text-xl text-center">Perhaps there’s a way your business could benefit by partnering with Iron Monkey? Please send us an email, and we’ll respond within 48 hours of receiving the inquiry.</p>
-
+      
       <div class="text-center mt-22">
+        <div class="mb-6 ">
+          <p class="text-lg sm:text-xl font-medium pb-4">Business Hours</p>
+          <p class="text-lg">Monday-Friday</p>
+          <p class="text-lg">10AM - 4PM</p>
+        </div>
         <div class="mb-6 ">
           <p class="text-lg sm:text-xl font-medium pb-4">Location</p>
           <p class="text-lg">North Las Vegas, NV</p>
         </div>
-        <div class="">
+        <div class="mb-10">
           <p class="text-lg sm:text-xl font-medium pb-4">Contact</p>
           <p class="text-lg">ironmonkeystudio702@gmail.com</p>
           <p class="text-lg">702-308-9874</p>
         </div>
       </div>
+
+      
     </section>
 
     
--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,13 @@
           <h1 class="sm:text-6xl text-3xl font-medium pb-4">数字原住民. 设计大师.</h1>
           <h1 class="sm:text-9xl text-5xl font-medium pb-10">Digital Natives. <br>Design Masters.</h1>
         </div>
-        <div class="mb-64">
+        <div class="mb-16">
           <p class="text-xl sm:text-2xl  w-3/4 sm:w-2/3">Iron Monkey is a <span class="font-semibold">web design,</span><span class="font-semibold"> branding</span>  and <span class="font-semibold">web development</span> agency that helps small businesses create brands and websites that customers love. We work with clients from around the world to make digital experiences that transform the lives of others.</p>
-          <NuxtLink to="/contact"><button class="mt-16 p-6 bg-black text-white">Start a project!</button></NuxtLink>
         </div>
-        
+        <div class="flex mt-16 mb-16">
+          <NuxtLink to="/contact"><button class="p-6 bg-black text-white text-center uppercase text-sm font-semibold">Start a project!</button></NuxtLink>
+         <a href="https://ironmonkey3116.setmore.com/" target="_blank"><button class="btn-padding ml-6 border-black border-2 text-black uppercase text-sm font-semibold">Book a free consultation</button></a>
+        </div>        
         
       </main> 
 
