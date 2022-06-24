2e1eee76407a273d60a4b01522f05436c331a066
Treana Slocum <treanaslocum.com>
2022-06-24 16:34:20 -0700

    Made book consultation btn responsive

--- pages/index.vue
+++ pages/index.vue
@@ -14,9 +14,9 @@
         <div class="mb-16">
           <p class="text-xl sm:text-2xl  w-3/4 sm:w-2/3">Iron Monkey is a <span class="font-semibold">web design,</span><span class="font-semibold"> branding</span>  and <span class="font-semibold">web development</span> agency that helps small businesses create brands and websites that customers love. We work with clients from around the world to make digital experiences that transform the lives of others.</p>
         </div>
-        <div class="flex mt-16 mb-16">
+        <div class="flex flex-wrap mt-16 mb-16">
           <NuxtLink to="/contact"><button class="p-6 bg-black text-white text-center uppercase text-sm font-semibold">Start a project!</button></NuxtLink>
-         <a href="https://ironmonkey3116.setmore.com/" target="_blank"><button class="btn-padding ml-6 border-black border-2 text-black uppercase text-sm font-semibold">Book a free consultation</button></a>
+         <a href="https://ironmonkey3116.setmore.com/" target="_blank"><button class="btn-padding sm:ml-6 mt-4 sm:mt-0 border-black border-2 text-black uppercase text-sm font-semibold">Book a free consultation</button></a>
         </div>        
         
       </main> 
