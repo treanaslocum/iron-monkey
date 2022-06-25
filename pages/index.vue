938e0925a1dda148d2fc0b9e76fb2d70d13e3b9d
Treana Slocum <treanaslocum.com>
2022-06-25 13:33:26 -0700

    Add tyoeform to contact page

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
           <h1 class="sm:text-9xl text-5xl font-medium pb-10">Digital Natives. <br>Design Masters.</h1>
         </div>
         <div class="mb-16">
-          <p class="text-xl sm:text-2xl  w-3/4 sm:w-2/3">Iron Monkey is a <span class="font-semibold">web design,</span><span class="font-semibold"> branding</span>  and <span class="font-semibold">web development</span> agency that helps small businesses create brands and websites that customers love. We work with clients from around the world to make digital experiences that transform the lives of others.</p>
+          <p class="text-xl sm:text-2xl  w-3/4 sm:w-2/3">Iron Monkey is a <span class="font-semibold">web design,</span><span class="font-semibold"> branding,</span>  and <span class="font-semibold">web development</span> agency that helps small businesses create brands and websites that customers love. We work with clients from around the world to make digital experiences that transform the lives of others.</p>
         </div>
         <div class="flex flex-wrap mt-16 mb-16">
           <NuxtLink to="/contact"><button class="p-6 bg-black text-white text-center uppercase text-sm font-semibold">Start a project!</button></NuxtLink>
