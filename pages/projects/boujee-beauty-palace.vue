976679004ce665d5a3caeb8e876d4aac0b3a0fbf
Treana Slocum <treanaslocum.com>
2022-07-16 10:30:55 -0700

    Change content on boujee beauty palace page to be more results focused.

--- pages/projects/boujee-beauty-palace.vue
+++ pages/projects/boujee-beauty-palace.vue
@@ -30,9 +30,9 @@
             <h5 class="text-lg font-semibold">Client</h5>
             <h3 class="mb-4">Boujee Beauty Palace is a burgeoning beauty business located in Las Vegas, Nevada. </h3>
             <h5 class="text-lg font-semibold">The Challenge</h5>
-            <h3 class="mb-4">Boujee Beauty Palace needed a clean layout that showcased their services. Their original website lacked professionalism and clear direction, so they wanted to revamp their visuals in an alluring way.</h3>
+            <h3 class="mb-4">Boujee Beauty Palace needed help making the appointment booking experience easier for their clients. Their original website lacked functionality and ease of use, so they wanted to make the website more performant and client friendly.</h3>
             <h5 class="text-lg font-semibold">The Solution</h5>
-            <h3 class="">We researched and designed the new Boujee Beauty Palace website to have a clean aesthetic in mind.</h3>
+            <h3 class="">We designed the new Boujee Beauty Palace website to be more appointment booking centric and upgraded the design as well to look more modern.</h3>
           </div>
           
         </div>
